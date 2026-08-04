edf5fc796eae8b49f58912771d907e24bc93f9f4
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2026-08-04 22:45:34 +0530

    Time: 1 ms (99.82%), Space: 46.3 MB (27.03%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    Integer[][] dp;
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+        int n = triangle.size();
+        dp = new Integer[n][n];
+
+        return solve(triangle, 0, 0);
+    }
+
+    private int solve(List<List<Integer>> triangle, int i, int j) {
+
+        if (i == triangle.size() - 1)
+            return triangle.get(i).get(j);
+
+        if (dp[i][j] != null)
+            return dp[i][j];
+
+        int down = solve(triangle, i + 1, j);
+        int diag = solve(triangle, i + 1, j + 1);
+
+        return dp[i][j] =
+                triangle.get(i).get(j) + Math.min(down, diag);
+    }
+}
